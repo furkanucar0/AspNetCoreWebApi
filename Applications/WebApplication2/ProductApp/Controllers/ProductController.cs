9d086ef19eeda73878f716bfda58deaa7203e814
Furkan_ucar <furkanucar229@gmail.com>
2023-08-09 01:00:25 +0300

    first commit

---
+++ Applications/WebApplication2/ProductApp/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductApp.Models;
+
+namespace ProductApp.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        public readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public IActionResult GetAllProducts() 
+        {
+            var products = new List<Product>()
+            {
+                new Product { Id = 1, ProductName = "Computer"},
+                new Product { Id = 2, ProductName = "Keyboard"}, 
+                new Product { Id = 3, ProductName = "Mouse"}
+            };
+            _logger.LogInformation("GetAllProducts actions has been called.");
+            return Ok(products);
+        }
+       
+        [HttpPost]
+        public IActionResult GetAllProducts([FromBody]Product product)
+        {
+            _logger.LogWarning("Product Has Been Created.");
+            return StatusCode(201); //Created
+        }
+    }
+}
--- Applications/WebApplication2/WebApplication2/Models/ResponseModel.cs
+++ Applications/WebApplication2/WebApplication2/Models/ResponseModel.cs
@@ -3,6 +3,6 @@
     public class ResponseModel
     {
         public int HttpStatus { get; set; }
-        public string Message { get; set; }
+        public string? Message { get; set; }
     }
 }
