5abb083f598f0877cbbcde4d62acb7233c2bbeef
Furkan_ucar <furkanucar229@gmail.com>
2023-08-08 16:01:34 +0300

    update first web api

---
+++ Applications/WebApplication2/WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    [ApiController]
+    [Route("home")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public ResponseModel GetMessage()
+        {
+            return new ResponseModel()
+            {
+                HttpStatus = 200,
+                Message = "Hello Asp.Net Core Web API"
+            };
+
+        }
+    }
+}
---
+++ Applications/WebApplication2/WebApplication2/Models/ResponseModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication2.Models
+{
+    public class ResponseModel
+    {
+        public int HttpStatus { get; set; }
+        public string Message { get; set; }
+    }
+}
